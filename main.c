78cb42cf30ff48d769902b50e73f6547d7d967bb
ronbarrantes <ronbarrantes@gmail.com>
2026-09-05 13:30:28 -0400

    Add a note, lol

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-// here is the plan
-//
-//
-//
+#include <stdint.h>
+typedef struct {
+  uint8_t pitch;
+  uint8_t velocity;
+  uint16_t duration;
+} Note;
+typedef struct {
+
+} Step;
+typedef struct {
+} Measure;
+typedef struct {
+} Pattern;
+typedef struct {
+} Song;
 
 int main(void) { return 0; }
