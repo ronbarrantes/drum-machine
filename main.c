bae5143d9a344eb6243d1be106573ee1dcae8157
ronbarrantes <ronbarrantes@gmail.com>
2026-09-05 16:10:31 -0400

    Make the measure

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdint.h>
+
+#define PPQN 96
+#define MAX_NOTES_PER_MEASURE 32
+
 typedef struct {
   uint8_t pitch;
   uint8_t velocity;
   uint16_t duration;
+  uint32_t start_tick;
 } Note;
-typedef struct {
 
-} Step;
 typedef struct {
+  Note notes[MAX_NOTES_PER_MEASURE];
+  uint8_t note_count;
+  uint8_t beats_per_measure;
+  uint8_t beat_unit;
 } Measure;
+
 typedef struct {
 } Pattern;
 typedef struct {
